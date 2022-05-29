e35912b3ea94618631c37062ae89919f3278cf07
Hortência Côrtes <hortencia.cortes@gmail.com>
2022-05-29 19:51:09 -0300

    open/close FAQ answers

--- script.js
+++ script.js
@@ -21,3 +21,22 @@ function initTabNav() {
   })
 }
 initTabNav();
+
+/*
+ * open/close FAQ answers 
+ */
+function initAccordion() {
+  const accordionList = document.querySelectorAll('.js-accordion dt');
+  accordionList[0].classList.add('ativo');
+  accordionList[0].nextElementSibling.classList.add('ativo');
+  
+  function activeAccordion() {
+    this.classList.toggle('ativo');
+    this.nextElementSibling.classList.toggle('ativo');
+  }
+  
+  accordionList.forEach(item => {
+    item.addEventListener('click', activeAccordion);
+  });
+}
+initAccordion();
