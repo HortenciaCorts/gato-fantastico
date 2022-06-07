eba56358eb622902e19182acefa5455639cfd7ae
Hortência Côrtes <hortencia.cortes@gmail.com>
2022-06-06 22:58:32 -0300

    module 3

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ function initTabNav() {
   const tabMenu = document.querySelectorAll('.js-tabmenu li');
   const tabContent = document.querySelectorAll('.js-tabcontent section');
   tabContent[0].classList.add('ativo');
-  console.log(tabContent[0])
 
   function activeTab(index) {
     tabContent.forEach(section => {
@@ -40,3 +39,48 @@ function initAccordion() {
   });
 }
 initAccordion();
+
+/*
+* scroll smooth in internal links 
+*/
+console.log('call')
+function initScrollSuave() {
+ const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+ function scrollToSection(event) {
+   event.preventDefault();
+   const href = event.currentTarget.getAttribute('href');
+   const section = document.querySelector(href);
+   section.scrollIntoView({
+     behavior: 'smooth',
+     block: 'start',
+   });
+ }
+ 
+ linksInternos.forEach(link => {
+   link.addEventListener('click', scrollToSection);
+ });
+}
+initScrollSuave();
+
+/*
+ * scroll animation
+ */
+function initScrollAnimation() {
+  const sections = document.querySelectorAll('.js-scroll');
+  const windowMetade = window.innerHeight * 0.6;
+  
+  function animaScroll() {
+    sections.forEach(section => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = (sectionTop - windowMetade) < 0;
+      if(isSectionVisible)
+        section.classList.add('ativo');
+      else 
+        section.classList.remove('ativo');
+    })
+  }
+  animaScroll();
+  
+  window.addEventListener('scroll', animaScroll)
+}
+initScrollAnimation();
